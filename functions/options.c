ffa699979d7d0e5b1c1ae1546fd9e54b56ee2aed
haxbash <bluemaquinajp@gmail.com>
2025-04-07 00:34:25 -0300

    creating optmizememoryram func

--- functions/options.c
+++ functions/options.c
@@ -13,3 +13,33 @@ void uninstallOneDrive() {
 }
 
 
+void optmizeMemoryRam() {
+
+    SetProcessWorkingSetSize(GetCurrentProcess(), -1, -1);
+    printf("[+] Memória do processo atual otimizada.\n");
+
+    DWORD processes[1024], cbNeeded, cProcesses;
+    if (!EnumProcesses(processes, sizeof(processes), &cbNeeded)) {
+        printf("[-] Falha ao enumerar processos.\n");
+        return 1;
+    }
+
+    cProcesses = cbNeeded / sizeof(DWORD);
+
+    for (unsigned int i = 0; i < cProcesses; i++) {
+        if (processes[i] == 0)
+            continue;
+
+        HANDLE hProcess = OpenProcess(PROCESS_SET_QUOTA | PROCESS_QUERY_INFORMATION, FALSE, processes[i]);
+        if (hProcess) {
+            if (SetProcessWorkingSetSize(hProcess, -1, -1)) {
+                printf("[+] Limpando memória do processo PID %lu\n", processes[i]);
+            }
+            CloseHandle(hProcess);
+        }
+    }
+
+    printf("[✔] Otimização de RAM finalizada.\n");
+    return 0;
+
+}
